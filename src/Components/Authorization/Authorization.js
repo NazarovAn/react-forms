c6d16adaec792fd494db3d538579180bd429d94f
NazarovAn <naatonchrome@gmail.com>
2020-11-02 16:35:02 +0400

    Registration and Authorization fetch

--- src/Components/Authorization/Authorization.js
+++ src/Components/Authorization/Authorization.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import './Authorization.css';
 
-function sayLoggedIn() {
-  console.log('Logged in');
+async function submitAuthorization(ev) {
+  ev.preventDefault()
+  const email = document.querySelector('.authorization__email').value;
+  const password = document.querySelector('.authorization__password').value;
+  const url = "https://internsapi.public.osora.ru/api/auth/login";
+  const formData = new FormData();
+  formData.append('email', email);
+  formData.append('password', password);
+
+  const response = await fetch(url, {
+    method: 'POST',
+    body: formData,
+  })
+
+  const json = await response.json();
+
+  if (json.status) {
+    const storageData = JSON.stringify({
+      access_token: json.data.access_token,
+      expires_at: json.data.expires_at,
+      token_type: json.data.token_type,
+    });
+
+    localStorage.setItem('Authorization result', storageData)
+    return;
+  }
+
+  console.log(json.errors);
 }
 
 export default function Authorization() {
   return (
     <form className="authorization__form">
       <h2 className="authorization__heading">Authorization</h2>
-      <input className="authorization__input email" type="text" placeholder="Email" required></input>
-      <input className="authorization__input password" type="text" placeholder="Password" required></input>
-      <button className="authorization__button" type="submit" onClick={ () => sayLoggedIn() }>Log in</button>
+      <input className="authorization__input authorization__email" type="email" placeholder="Email" required></input>
+      <input className="authorization__input authorization__password" type="password" placeholder="Password" required></input>
+      <button className="authorization__button" type="submit" onClick={ (ev) => submitAuthorization(ev) }>Log in</button>
     </form>
   )
 }
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import './Registration.css';
 
-function saySignedIn() {
-  console.log('Signed in');
+async function submitAuthorization(ev) {
+  ev.preventDefault();
+  const name = document.querySelector('.registration__name').value;
+  const email = document.querySelector('.registration__email').value;
+  const password = document.querySelector('.registration__password').value;
+  const confirmation = document.querySelector('.registration__password_confirmation').value;
+
+  const url = 'https://internsapi.public.osora.ru/api/auth/signup';
+  const formData = new FormData();
+  formData.append('name', name);
+  formData.append('email', email);
+  formData.append('password', password);
+  formData.append('password_confirmation', confirmation)
+
+  const response = await fetch(url, {
+    method: 'POST',
+    body: formData,
+  })
+
+  const json = await response.json();
+
+  if (json.status) {
+    console.log(json.data.message);
+    return;
+  }
+  
+  console.log(json.errors);
 }
 
 export default function Registration() {
   return (
     <form className="registration__form">
       <h2 className="registration__heading">Registration</h2>
-      <input type="text" className="registration__input name" placeholder="Name" required></input>
-      <input type="text" className="registration__input email" placeholder="Email" required></input>
-      <input type="text" className="registration__input password" placeholder="Password" required></input>
-      <input type="text" className="registration__input password_confirmation" placeholder="Confirm Password" required></input>
-      <button type="submit" className="registration__button" onClick={ () => saySignedIn() }>Sign in</button>
+      <input type="text" className="registration__input registration__name" placeholder="Name"></input>
+      <input type="email" className="registration__input registration__email" placeholder="Email"></input>
+      <input type="password" className="registration__input registration__password" placeholder="Password"></input>
+      <input type="password" className="registration__input registration__password_confirmation" placeholder="Confirm Password"></input>
+      <button type="submit" className="registration__button" onClick={ (ev) => submitAuthorization(ev) }>Sign in</button>
     </form>
   )
 }
